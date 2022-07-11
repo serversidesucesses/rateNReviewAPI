195115120c87f9f69d99af80fbfc8b789e36aa89
Tsering Dingtsa <kesangdingtsa@gmail.com>
2022-07-11 11:07:04 -0700

    backend skeleton

--- client/src/Components/Q&A/questions.jsx
+++ client/src/Components/Q&A/questions.jsx
@@ -1,1 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function QuestionList() {
+  useState();
+
+  //useEffect componentDidMount() --> get the data for questions and answers 
+  return (
+    <div> </div>
+  );
+}
