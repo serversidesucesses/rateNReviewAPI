49ec2273b0cf3bd08e6f7f71c940a51a9aa892fb
rochun <royce.h.chun@gmail.com>
2022-07-13 15:08:53 -0700

    fix gitignore bug

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,29 +1,39 @@
 require('dotenv').config();
 const axios = require('axios');
-
+// need to require the .env file here
+// const url = `https://app-hrseei-api.herokuapp.com/api/fec2/hr-rfp`;
 axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
-axios.defaults.headers.common['Authorization'] = process.env.API_KEY;
+axios.defaults.headers.common['Authorization'] = process.env.gitToken;
 
 exports.getProductDetails = (req, res) => {
   console.log('product id is:', req.query.id);
   axios.get(`/products/${req.query.id}`)
-    .then((response) => (res.status(200).send(response.data)))
+    .then((response) =>
+      (
+        res.status(200).send(response.data)
+        // console.log(JSON.stringify(response.data))
+      )
+    )
+    .catch((error) => {
+      //console.log(error);
+      res.status(400).send(error);
+    });
+}
+
+
+exports.getProductStyles = (req, res) => {
+  //${req.query.product_id}
+  axios.get(`/products/${req.query.id}/styles`)
+    .then(
+      (response) => {res.status(200).send(response.data);
+    })
     .catch((error) => {
       res.status(400).send(error);
     });
-};
+ };
 
-exports.getProductStyles = (req, res) => {
-  axios.get(`/products/${req.query.id}/styles`)
-    .then(
-      (response) => {res.status(200).send(response.data); },
-    )
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-};
-
-exports.handleClick = (req, res) => {
+ exports.handleClick = (req, res) => {
+  //${req.query.product_id}
   const params = {
     element: data.element,
     widget: data.widget,
@@ -33,21 +43,24 @@ exports.handleClick = (req, res) => {
     method: 'POST',
     url: '/interactions',
     data: params,
-  };
+  }
   axios.post(options)
     .then(
-      console.log('sucessfully post click interaction'),
-    )
+      // (response) => {res.status(200).send(response.data);}
+      console.log('sucessfully post click interaction')
+      )
     .catch((error) => {
       res.status(400).send(error);
     });
-};
+ };
+
+
 
 exports.addToCart = (req, res) => {
   console.log(JSON.stringify(req.body));
   axios.post(`/cart`, req.body)
     .then((response) => res.status(200).send(response.data))
     .catch((err) => {
-      res.status(400).send(err);
+      res.status(400).send(error);
     });
-};
+ };
