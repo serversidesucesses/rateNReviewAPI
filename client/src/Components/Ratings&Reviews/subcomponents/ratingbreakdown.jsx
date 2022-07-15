8988d36862b1ef9225e3365f77a14fc2d4b5096a
rochun <royce.h.chun@gmail.com>
2022-07-14 23:13:51 -0700

    Create ratingbreak down file

--- client/src/Components/Ratings&Reviews/subcomponents/ratingbreakdown.jsx
+++ client/src/Components/Ratings&Reviews/subcomponents/ratingbreakdown.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function RatingBreakdown() {
   const [overallRating, setOverallRating] = useState([]);
   const [ratings, setRatings] = useState({});
+
+  useEffect(() => {
+    axios.get(``)
+  }, []);
 }
+
