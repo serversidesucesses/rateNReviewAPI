8f66bafdd9ff1d0d73864c5ccfcb2bf838ecab5a
River Xiang <rxlbas@gmail.com>
2022-07-20 14:23:12 -0700

    move globalstyle lines

--- client/src/Components/Styles/globalstyle.styled.js
+++ client/src/Components/Styles/globalstyle.styled.js
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  *{
+  body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-  body {
     overflow: ${(props) => props.overflow || 'auto'};
   }
 `;
