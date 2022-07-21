439eb6193f72ed45f59fdd4626c8c21f235913f7
rochun <77751021+rochun@users.noreply.github.com>
2022-07-21 16:42:01 -0700

    Delete _tests_ directory

--- _tests_/app.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render, shallow } from '@testing-library/react';
-import App from '../client/src/index.jsx';
-
-test('renders without crashing', () => {
-  render(<App/>);
-});
--- _tests_/questions.test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {render, screen, getByLabelText} from '@testing-library/react';
-import {toBeTruthy} from '@testing-library/jest-dom';
-import SeeMoreQuestionStyled from '../Components/Q&A/questions.jsx';
-
